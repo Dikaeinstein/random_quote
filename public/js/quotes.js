68f1cda8d2c35f9c0157f88b76f3c43fcaded2bf
Dikaeinstein <Dikaeinstein@users.noreply.github.com>
2018-03-19 00:58:39 +0100

    refactored to code to use vanilla css and js

--- public/js/quotes.js
+++ public/js/quotes.js
@@ -1,81 +1,129 @@
-/* jshint esversion: 6 */
-function success (quotes) {
-    // variable declarations
-    let colors = [
-        '#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', 
-        '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857"
-    ];
-    let quoIdx = Math.floor(Math.random() * quotes.length);
-    let colIdx = Math.floor(Math.random() * colors.length);
-    let randomQuote = quotes[quoIdx].quoteText;
-    let author = quotes[quoIdx].quoteAuthor;
+const displayQuotes = (quotes) => {
+  // variable declarations
+  const colors = [
+    '#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6',
+    '#FB6964', '#342224', '#472E32', '#BDBB99', '#77B1A9', '#73A857'
+  ];
+  const quoIdx = Math.floor(Math.random() * quotes.length);
+  const colIdx = Math.floor(Math.random() * colors.length);
+  const randomQuote = quotes[quoIdx].quoteText;
+  const author = quotes[quoIdx].quoteAuthor;
+  const color = colors[colIdx];
 
-    // get current twitter tweet intent url
-    let twitterLink = $("#twitter").attr("href").slice(0, 75) + '"' + randomQuote + '" ' + author;
-    // set current twitter url
-    $("#twitter").attr("href", twitterLink);
+  const body = document.getElementsByTagName('body')[0];
+  const banner = document.getElementsByClassName('banner')[0];
+  const newQuote = document.getElementById('new-quote');
+  const addQuote = document.getElementById('add-quote');
+  const saveQuote = document.getElementById('save-quote');
+  const quoteForm = document.getElementsByTagName('form')[0];
+  const quoteText = document.getElementById('text');
+  const quoteAuthor = document.getElementsByClassName('quote-author')[0];
+  const quoteFormSection = document.getElementsByClassName('quote-form-section')[0]
 
-    // animating colors
-    $("body").animate({
-        backgroundColor: colors[colIdx],
-        color: colors[colIdx]
-    }, 1000);
-    $(".banner").animate({
-        color: colors[colIdx]
-    });
-    $("#new-quote").animate({
-         backgroundColor: colors[colIdx]
-    }, 1000);
-    $(".buttons .button").animate({
-        backgroundColor: colors[colIdx],
-    }, 1000);
-    
-    // fadeOut/fadeIn quote text
-    $("#text").hide().html(randomQuote).fadeIn(1000);
-    $(".quote-author").hide().html(author).fadeIn(1000);
-}
+  // Animate background color change
+  const animateColor = () => {
+    body.style.backgroundColor = color;
+    body.style.color = color;
+    banner.style.color = color;
+    newQuote.style.backgroundColor = color;
+    addQuote.style.backgroundColor = color;
+    saveQuote.style.color = color;
+  };
 
+  // Show quote and author
+  const showQuote = () => {
+    quoteText.style.visibility = 'visible';
+    quoteText.innerHTML = randomQuote;
+    quoteAuthor.style.visibility = 'visible';
+    quoteAuthor.innerHTML = author;
+  };
 
-$(document).ready(function () {
+  // Add quote
+  addQuote.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (quoteFormSection.style.visibility === 'hidden') {
+      quoteFormSection.style.visibility = 'visible';
+    }
+  });
+
+  // Save quote
+  quoteForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const { quote, author } = quoteForm;
+    if (quote.value === '' || author.value === '') {
+      return;
+    }
+    quoteFormSection.style.visibility = 'hidden';
+
+    quotes.push({ quoteText: quote.value, quoteAuthor: `- ${author.value}` });
+    quote.value = '';
+    author.value = '';
+  });
   
-    let quotes = [{
-        "quoteText": "Genius is one percent inspiration and ninety-nine percent perspiration.",
-        "quoteAuthor": "- Thomas Edison"
-        }, {
-"quoteText": "You can observe a lot just by watching.",
-"quoteAuthor": "- Yogi Berra"
-}, {
-"quoteText": "A house divided against itself cannot stand.",
-"quoteAuthor": "- Abraham Lincoln"
-}, {
-"quoteText": "Difficulties increase the nearer we get to the goal.",
-"quoteAuthor": "- Johann Wolfgang von Goethe"
-}, {
-"quoteText": "Fate is in your hands and no one elses",
-"quoteAuthor": "- Byron Pulsifer"
-}, {
-"quoteText": "Be the chief but never the lord.",
-"quoteAuthor": "- Lao Tzu"
-}, {
-"quoteText": "Nothing happens unless first we dream.",
-"quoteAuthor": "- Carl Sandburg"
-}, {
-"quoteText": "Well begun is half done.",
-"quoteAuthor": "- Aristotle"
-}, {
-"quoteText": "Life is a learning experience, only if you learn.",
-"quoteAuthor": "- Yogi Berra"
-}, {
-"quoteText": "Self-complacency is fatal to progress.",
-"quoteAuthor": "- Margaret Sangster"
-}, {
-"quoteText": "Peace comes from within. Do not seek it without.",
-"quoteAuthor": "- Buddha"
-}];
+  quoteFormSection.style.visibility = 'hidden';
+  animateColor();
+  showQuote();
+};
 
-success(quotes);
+// Event listner for setting up app once DOM content have been parsed
+// Used 'DOMContentLoaded' event to avoid flashes of hidden content on page load
+window.addEventListener('DOMContentLoaded', () => {
+  const quotes = [
+    {
+      quoteText: 'Genius is one percent inspiration and ninety-nine percent perspiration.',
+      quoteAuthor: '- Thomas Edison'
+    },
+    {
+      quoteText: 'You can observe a lot just by watching.',
+      quoteAuthor: '- Yogi Berra'
+    },
+    {
+      quoteText: 'A house divided against itself cannot stand.',
+      quoteAuthor: '- Abraham Lincoln'
+    },
+    {
+      quoteText: 'Difficulties increase the nearer we get to the goal.',
+      quoteAuthor: '- Johann Wolfgang von Goethe'
+    },
+    {
+      quoteText: 'Fate is in your hands and no one elses',
+      quoteAuthor: '- Byron Pulsifer'
+    },
+    {
+      quoteText: 'Be the chief but never the lord.',
+      quoteAuthor: '- Lao Tzu'
+    },
+    {
+      quoteText: 'Nothing happens unless first we dream.',
+      quoteAuthor: '- Carl Sandburg'
+    },
+    {
+      quoteText: 'Well begun is half done.',
+      quoteAuthor: '- Aristotle'
+    },
+    {
+      quoteText: 'Life is a learning experience, only if you learn.',
+      quoteAuthor: '- Yogi Berra'
+    }, 
+    {
+      quoteText: 'Self-complacency is fatal to progress.',
+      quoteAuthor: '- Margaret Sangster'
+    },
+    {
+      quoteText: 'Peace comes from within. Do not seek it without.',
+      quoteAuthor: '- Buddha'
+    },
+    {
+      quoteText: 'Brilliance is evenly distributed but opportunity is not.',
+      quoteAuthor: '- Andela'
+    }
+  ];
 
-    $("#new-quote").on("click", function () {
-        success(quotes);
-    });
+  displayQuotes(quotes);
+
+  const newQuote = document.getElementById('new-quote');
+
+  newQuote.addEventListener('click', () => {
+    displayQuotes(quotes);
+  });
 });
